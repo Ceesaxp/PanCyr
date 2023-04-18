dccc7c2d831606bfa63251b3a064c2b1a1e97025
Andrei Popov <ceesaxp@gmail.com>
2023-04-18 06:40:05 +0200

    Initial Commit

---
+++ PanCyr/PanCyrApp.swift
@@ -0,0 +1,17 @@
+//
+//  PanCyrApp.swift
+//  PanCyr
+//
+//  Created by Andrei Popov on 18.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct PanCyrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
